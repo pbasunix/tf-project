c6d48cf2cd2204357e7593d0ca98761595c7097b
Prak Roma <pba.sunix@gmail.com>
2020-03-24 18:55:55 -0400

    Added AWS resource for provisioning: IGW

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -38,91 +38,93 @@ resource "aws_subnet" "privateSN" {
 # Public Route Table
 
 resource "aws_route_table" "publicRT" {
-  vpc_id = aws_vpc.demoVPC.id
+    vpc_id = aws_vpc.demoVPC.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    #gateway_id = aws_internet_gateway.igw.id
+    route {
+    	cidr_block = "0.0.0.0/0"
+    	gateway_id = aws_internet_gateway.igw.id
   }
 
-  tags = {
-    Name = "Public Route Table"
+    tags = {
+    	Name = "Public Route Table"
   }
 }
 
 # Associate Public route table to public subnet
 
 resource "aws_route_table_association" "publicRTA" {
-  subnet_id      = aws_subnet.publicSN.id
-  route_table_id = aws_route_table.publicRT.id
+    subnet_id      = aws_subnet.publicSN.id
+    route_table_id = aws_route_table.publicRT.id
 }
 
-
+/*
 # Private Route Table
 
 resource "aws_route_table" "privateRT" {
-  vpc_id = aws_vpc.demoVPC.id
+    vpc_id = aws_vpc.demoVPC.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    # gateway_id = aws_internet_gateway.igw.id
+    route {
+    	cidr_block = "0.0.0.0/0"
+    	nat_gateway_id = aws_nat_gateway.natGW.id
   }
 
-  tags = {
-    Name = "Private Route Table"
+    tags = {
+    	Name = "Private Route Table"
   }
 }
+
 
 # Associate Private table to public subnet
 
 resource "aws_route_table_association" "privateRTA" {
-  subnet_id      = aws_subnet.privateSN.id
-  route_table_id = aws_route_table.privateRT.id
+    subnet_id      = aws_subnet.privateSN.id
+    route_table_id = aws_route_table.privateRT.id
 }
 
+*/
 
 # Internet Gateway
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.demoVPC.id
+    vpc_id = aws_vpc.demoVPC.id
 
-  tags = {
-    Name = "Internet Gateway"
+    tags = {
+        Name = "Internet Gateway"
   }
 }
 
-# Public Security Group -- inbound and outbound rules
+# Public Security Group
 
 resource "aws_security_group" "webSG" {
-  name        = "public"
-  description = "Allow SSH, HTTP traffic"
+    name        = "public"
+    description = "Allow SSH, HTTP traffic"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    ingress {
+    	from_port   = 80
+    	to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    ingress {
+    	from_port   = 443
+    	to_port     = 443
+    	protocol    = "tcp"
+    	cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    ingress {
+    	from_port   = -1
+    	to_port     = -1
+    	protocol    = "icmp"
+    	cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    ingress {
+    	from_port   = 22
+    	to_port     = 22
+    	protocol    = "tcp"
+    	cidr_blocks = ["0.0.0.0/0"]
   }
 
   vpc_id = aws_vpc.demoVPC.id
@@ -135,34 +137,51 @@ resource "aws_security_group" "webSG" {
 # Private Security Group
 
 resource "aws_security_group" "dbSG" {
-  name        = "sg_test_web"
-  description = "Allow DB, SSH, ICMP traffic from public subnet in defined VPC"
+    name        = "sg_test_web"
+    description = "Allow DB, SSH, ICMP traffic from public subnet in defined VPC"
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = [var.public_sn_cidrr]
+    ingress {
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        cidr_blocks = [var.public_sn_cidr]
   }
 
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = [var.public_sn_cidr]
-  }
+    ingress {
+    	from_port   = -1
+	to_port     = -1
+	protocol    = "icmp"
+    	cidr_blocks = [var.public_sn_cidr]
+  } 
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.public_sn_cidr]
+    ingress {
+    	from_port   = 22
+    	to_port     = 22
+    	protocol    = "tcp"
+    	cidr_blocks = [var.public_sn_cidr]
   }
 
   vpc_id = aws_vpc.demoVPC.id
 
-  tags = {
-    Name = "dbSG"
-  }
+    tags = {
+        Name = "dbSG"
+    }
 }
 
+
+/*
+resource "aws_nat_gateway" "natGW" {
+    allocation_id = aws_eip.nat.id
+    subnet_id     = aws_subnet.publicSN.id
+   
+    tags = {
+        Name = "NAT Gateway"
+    }
+}
+
+
+resource "aws_eip" "elasticIP" {
+    instance = aws_n
+    vpc      = true
+}
+*/
